63b74440c917190b7208c1d2c0bfc60d21b68814
IyanTRICKSTER <akiyan2002@gmail.com>
2023-07-04 22:34:38 +0700

    Fix: enable referrer policy

--- pkg/domains/auth/middleware/CORS.go
+++ pkg/domains/auth/middleware/CORS.go
@@ -3,6 +3,7 @@ package middleware
 import "github.com/gin-gonic/gin"
 
 func HandleCORS(c *gin.Context) {
+	c.Header("Referrer-Policy", "no-referrer-when-downgrade")
 	c.Header("Access-Control-Allow-Origin", "*")
 	c.Header("Access-Control-Allow-Methods", "*")
 	c.Header("Access-Control-Allow-Headers", "*")
